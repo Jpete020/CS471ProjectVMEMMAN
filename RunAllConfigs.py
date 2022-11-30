093006536d780f7d8140b4dcc49d0067301cb347
dankoo97 <dak.7997@gmail.com>
2022-11-30 17:12:10 -0500

    fixed bug in RunAllConfigs.py if output.txt does not exist

--- RunAllConfigs.py
+++ RunAllConfigs.py
@@ -1,6 +1,9 @@
 import os
 
-os.remove("output.txt")
+try:
+    os.remove("output.txt")
+except FileNotFoundError:
+    pass
 for pageSize in (512, 1024, 2048):
     for frameCount in (4, 8, 12):
         os.system(f"java -jar build/libs/CS471ProjectVMEMMAN-1.0-SNAPSHOT.jar input.txt {pageSize} {frameCount} >> output.txt")
