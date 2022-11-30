fad11e8bf8b5a386d46123b59c6dd98487f9c143
Jpete020 <98491629+Jpete020@users.noreply.github.com>
2022-11-30 14:45:06 -0500

    added file reader by line

--- src/main/java/vmemman/VmemmanDriver.java
+++ src/main/java/vmemman/VmemmanDriver.java
@@ -1,8 +1,30 @@
 package vmemman;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class VmemmanDriver {
 
     public static void main(String[] args){
-        System.out.println("HELLOW WORLD");
+        BufferedReader reader;
+        BufferedWriter writer;
+        String inputFile = args[0];
+
+        try {
+            reader = new BufferedReader(new FileReader(inputFile));
+            String line = reader.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                // read next line
+                line = reader.readLine();
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
